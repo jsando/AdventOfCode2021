b3f2c6dc15a5ff4fd93d5e7d5d225a9afc707a3f
jsando <jason.sando.lv@gmail.com>
2022-01-20 14:31:33 -0800

    day 15 part 1

--- day15/solution.go
+++ day15/solution.go
@@ -1,13 +1,21 @@
 package day15
 
 import (
+	"container/heap"
 	"fmt"
 	"github.com/jsando/AdventOfCode2021/helpers"
 	"image"
 )
 
 func Run(inputpath string) {
+	fmt.Printf("Part 1: %d\n", part1(inputpath)) // 578 too low, added only right/down constraint and now 583 is correct
+}
 
+func part1(inputpath string) int {
+	grid := NewGridFromFile(inputpath)
+	path := grid.Path(image.Point{X: 0, Y: 0}, image.Point{X: grid.Width() - 1, Y: grid.Height() - 1})
+	grid.PrintPath(path)
+	return grid.PathCost(path)
 }
 
 type RiskMap struct {
@@ -41,20 +49,25 @@ func (r *RiskMap) Height() int {
 }
 
 func (r *RiskMap) Path(start image.Point, goal image.Point) []image.Point {
-	var frontier []image.Point
-	frontier = append(frontier, start)
+	frontier := make(PriorityQueue, 0)
+	frontier.PushPoint(start, 0)
 	cameFrom := map[image.Point]image.Point{
 		start: {},
 	}
+	costSoFar := map[image.Point]int{
+		start: 0,
+	}
 	for len(frontier) > 0 {
-		current := frontier[0]
+		current := frontier.PopPoint()
 		if current == goal {
 			break
 		}
-		frontier = frontier[1:]
 		for _, next := range r.Neighbors(current) {
-			if _, ok := cameFrom[next]; !ok {
-				frontier = append(frontier, next)
+			newCost := costSoFar[current] + r.CostOf(next)
+			if _, ok := costSoFar[next]; !ok || newCost < costSoFar[next] {
+				costSoFar[next] = newCost
+				priority := newCost + heuristic(goal, next)
+				frontier.PushPoint(next, priority)
 				cameFrom[next] = current
 			}
 		}
@@ -69,17 +82,29 @@ func (r *RiskMap) Path(start image.Point, goal image.Point) []image.Point {
 	return path
 }
 
+func (r *RiskMap) PathCost(path []image.Point) int {
+	cost := -1
+	for _, p := range path {
+		cost += r.CostOf(p)
+	}
+	return cost
+}
+
+func heuristic(p1, p2 image.Point) int {
+	return helpers.AbsInt(p1.X-p2.X) + helpers.AbsInt(p1.Y-p2.Y)
+}
+
 func (r *RiskMap) Neighbors(p image.Point) []image.Point {
 	n := make([]image.Point, 0, 4)
-	if p.X > 0 {
-		n = append(n, image.Point{X: p.X - 1, Y: p.Y})
-	}
+	//if p.X > 0 {
+	//	n = append(n, image.Point{X: p.X - 1, Y: p.Y})
+	//}
 	if p.X < r.Width()-1 {
 		n = append(n, image.Point{X: p.X + 1, Y: p.Y})
 	}
-	if p.Y > 0 {
-		n = append(n, image.Point{X: p.X, Y: p.Y - 1})
-	}
+	//if p.Y > 0 {
+	//	n = append(n, image.Point{X: p.X, Y: p.Y - 1})
+	//}
 	if p.Y < r.Height()-1 {
 		n = append(n, image.Point{X: p.X, Y: p.Y + 1})
 	}
@@ -91,12 +116,15 @@ func (r *RiskMap) PrintPath(path []image.Point) {
 	for _, row := range r.grid {
 		rowCopy := []rune{}
 		for _, ch := range row {
-			rowCopy = append(rowCopy, ch)
+			if ch == 0 {
+				panic("just kidding")
+			}
+			rowCopy = append(rowCopy, ' ')
 		}
 		grid = append(grid, rowCopy)
 	}
 	for _, p := range path {
-		grid[p.Y][p.X] = '*'
+		grid[p.Y][p.X] = r.grid[p.Y][p.X]
 	}
 	fmt.Printf("\nFound path:\n")
 	for _, row := range grid {
@@ -105,4 +133,58 @@ func (r *RiskMap) PrintPath(path []image.Point) {
 		}
 		fmt.Printf("\n")
 	}
+}
+
+func (r *RiskMap) CostOf(point image.Point) int {
+	return int(r.grid[point.Y][point.X] - '0')
+}
+
+// Copy-paste from Go standard library example for 'heap'.
+
+// An Item is something we manage in a priority queue.
+type Item struct {
+	value    image.Point // The value of the item; arbitrary.
+	priority int         // The priority of the item in the queue.
+	index    int         // The index of the item in the heap (maintained by the heap.Interface methods)
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) PushPoint(p image.Point, cost int) {
+	heap.Push(pq, &Item{value: p, priority: cost})
+}
+
+func (pq *PriorityQueue) PopPoint() image.Point {
+	item := heap.Pop(pq).(*Item)
+	return item.value
 }
--- day15/solution_test.go
+++ day15/solution_test.go
@@ -13,6 +13,20 @@ func TestExample(t *testing.T) {
 	if grid.Height() != 10 {
 		t.Errorf("expected height of 10, got %d", grid.Height())
 	}
-	path := grid.Path(image.Point{X: 0, Y: 0}, image.Point{X: 9, Y: 9})
-	grid.PrintPath(path)
+	if grid.CostOf(image.Point{0, 0}) != 1 {
+		t.Errorf("wrong value")
+	}
+	if grid.CostOf(image.Point{1, 1}) != 3 {
+		t.Errorf("wrong value")
+	}
+	if grid.CostOf(image.Point{9, 9}) != 1 {
+		t.Errorf("wrong value")
+	}
+}
+
+func TestPart1(t *testing.T) {
+	cost := part1("example-input.txt")
+	if cost != 40 {
+		t.Errorf("expected cost of 40, got %d", cost)
+	}
 }
